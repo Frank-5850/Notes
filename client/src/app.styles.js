d09f62ddb9b5d16ca76396912e2aa267daefb391
franco <phanfranco@yahoo.com>
2022-08-04 23:32:29 -0700

    added some styling to front-end header and body

---
+++ client/src/app.styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 25px;
+  font-size: 2em;
+`;
+
+export const NotesContainer = styled.div`
+  display: flex;
+  padding: 50px;
+  border: 1px solid green;
+`;
